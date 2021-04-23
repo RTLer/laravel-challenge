adcf71bd133163456217a2da82623ccbd95731a0
navid <navidsafavico@gmail.com>
2021-04-23 11:05:06 +0400

    add route for transaction + add controllers + add routes for get and create transaction

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'transactions'],function(){
+    Route::get('/', ['App\Http\Controllers\API\TransactionController', 'index']);
+    Route::post('/{type}', ['App\Http\Controllers\API\TransactionController', 'create']);
 });
